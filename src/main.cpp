a32c6e79e70e8a8724d310758ccc21696158d23e
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2020-04-28 10:42:05 +0200

    Updated 28.04.2020

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,55 @@
 #include <Arduino.h>
 const unsigned long TIME_WAIT = 5000;
-const int LED_R = D6;
-const int LED_Y = D7;
-const int LED_G = D8;
+
+const int LEDS[3] = {D6, D7, D8}; //rot, gelb, gruen
 
 unsigned long millis_current;
 unsigned long millis_previous = 0;
 unsigned int state;
 
-void schalteAmpel(bool rot, bool gelb, bool gruen);
+void schalteAmpel(bool led_states[3]);
+
 void setup() {
-  Serial.begin(9600);  
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_Y, OUTPUT);
-  pinMode(LED_G, OUTPUT);
-  schalteAmpel(LOW, LOW, LOW);
+  bool led_states[3] = {LOW, LOW, LOW};
+  Serial.begin(9600); 
+  Serial.printf("Anzahl Felder in LEDS: %i\n", sizeof(LEDS)/sizeof(int)); 
+  for(unsigned int i = 0; i < sizeof(LEDS)/sizeof(int); i++) {
+    pinMode(LEDS[i], OUTPUT);
+  }
+  schalteAmpel(led_states);
   state = 0;
 }
 
 void loop() {
+  bool led_states[3];
   millis_current = millis();
   if(millis_current - millis_previous >= TIME_WAIT) {
     Serial.printf("Zustand: %i\n", state);
     switch(state){
       case 0: {
         //Rot
-        schalteAmpel(HIGH, LOW, LOW);
+        led_states[0] = HIGH;
+        led_states[1] = LOW;
+        led_states[2] = LOW;
+        schalteAmpel(led_states);
         state = 1;
         break;
       }
       case 1: {
         //Rot-Gelb
-        schalteAmpel(HIGH, HIGH, LOW);
+        schalteAmpel(led_states);
         state = 2;
         break;
       }
       case 2: {
         //Grün
-        schalteAmpel(LOW, LOW, HIGH);
+        schalteAmpel(led_states);
         state = 3;
         break;
       }
       case 3: {
         //Gelb
-        schalteAmpel(LOW, HIGH, LOW);
+        schalteAmpel(led_states);
         state = 0;
         break;
       }
@@ -52,8 +58,8 @@ void loop() {
   }
 }
 
-void schalteAmpel(bool rot, bool gelb, bool gruen) {
-  digitalWrite(LED_R, rot);
-  digitalWrite(LED_Y, gelb);
-  digitalWrite(LED_G, gruen);
+void schalteAmpel(bool led_states[3]) {
+  for(unsigned int i = 0; i < sizeof(LEDS)/sizeof(int); i++) {
+    digitalWrite(LEDS[i], led_states[i]);
+  }
 }
